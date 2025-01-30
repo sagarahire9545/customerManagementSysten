6a4c9d79c51f885f707624574288ac2ec87f1115
sagara <sahire@1gen.io>
2025-01-30 14:17:49 +0530

    swagger configuration

---
+++ src/main/java/com/custManagent/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.custManagent.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+
+//        final String securitySchemeName = "bearerAuth";
+//        return new OpenAPI()
+//                .addSecurityItem(new SecurityRequirement()
+//                        .addList(securitySchemeName))
+//                .components(new Components()
+//                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+//                                .name(securitySchemeName)
+//                                .type(SecurityScheme.Type.HTTP)
+//                                .scheme("bearer")
+//                                .bearerFormat("JWT")));
+
+        final String securitySchemeName = "jwtToken";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")
+                                .description("JWT Token")));
+    }
+
+}
